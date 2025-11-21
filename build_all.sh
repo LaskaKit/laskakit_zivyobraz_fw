de817da7825cada6b36424da66cf1b3ae5cbe7a9
Patrik Novak <novakpatrik.99@protonmail.com>
2025-11-21 12:38:01 +0100

    Update submodules, fail workflow when compilation fails.
    - laskakit_epaper -> fixed 12 inch epaper
    - laskakit_zivyobraz_client -> urlencoding

--- build_all.sh
+++ build_all.sh
@@ -9,5 +9,8 @@ for BOARD in $BOARDS; do
     for DISPLAY in $DISPLAYS; do
         echo "Building Zivyobraz for $DISPLAY $BOARD"
         DISPLAY_TYPE="DISPLAY_$DISPLAY" pio run -e "$BOARD"
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
     done
 done
