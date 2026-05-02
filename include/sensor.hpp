e70e1cab75bc191f11718bb38a9b1cc555fea591
Patrik Novák <novakpatrik.99@protonmail.com>
2026-05-02 21:05:00 +0200

    Implement I2C bus scan. Remove BH1750.
    I2C bus gets scanned before any reading is performed.
    Only possible sensor addresses are scanned.
    
    - BH1750 does not have much sense
    
    Issue #9

--- include/sensor.hpp
+++ include/sensor.hpp
@@ -5,80 +5,123 @@
 #include <SparkFun_SCD4x_Arduino_Library.h>
 #include <SensirionI2cStcc4.h>
 #include <SensirionI2CSgp41.h>
-#include <BH1750.h>
+#include <Wire.h>
 #include <esp_log.h>
 
-SensirionI2cSht4x sht4;
-Adafruit_BME280 bme;
-SCD4x SCD4(SCD4x_SENSOR_SCD41);
-SensirionI2cStcc4 stcc4;
-SensirionI2CSgp41 sgp41;
-BH1750 bh1750;
+namespace {
+    SensirionI2cSht4x sht4;
+    Adafruit_BME280 bme;
+    SCD4x SCD4(SCD4x_SENSOR_SCD41);
+    SensirionI2cStcc4 stcc4;
+    SensirionI2CSgp41 sgp41;
+}
 
 namespace LaskaKit::ZivyObraz {
-    enum Sensor
-    {
-        _NONE = 0,
-        _SHT4x = 1,
-        _BME280 = 1 << 1,
-        _SCD4x = 1 << 2,
-        _STCC4 = 1 << 3,
-        _SGP41 = 1 << 4,
-        _BH1750 = 1 << 5
+
+    enum class Sensor : uint8_t {
+        S_SHT4x  = 0,
+        S_BME280 = 1,
+        S_SCD4x  = 2,
+        S_STCC4  = 3,
+        S_SGP41  = 4,
+        S_COUNT
     };
 
-    struct SHT4xReading
-    {
-        double temperature = 0;
-        double humidity = 0;
+    // Separate type for the detected-set bitmask
+    using SensorMask = uint8_t;
+
+    constexpr SensorMask sensorBit(Sensor s) {
+        return static_cast<SensorMask>(1u << static_cast<uint8_t>(s));
+    }
+
+    constexpr SensorMask MASK_NONE   = 0;
+    constexpr SensorMask MASK_SHT4x  = sensorBit(Sensor::S_SHT4x);
+    constexpr SensorMask MASK_BME280 = sensorBit(Sensor::S_BME280);
+    constexpr SensorMask MASK_SCD4x  = sensorBit(Sensor::S_SCD4x);
+    constexpr SensorMask MASK_STCC4  = sensorBit(Sensor::S_STCC4);
+    constexpr SensorMask MASK_SGP41  = sensorBit(Sensor::S_SGP41);
+
+
+    struct SensorEntry {
+        Sensor      sensor;
+        uint8_t     address;
     };
 
-    struct BME280Reading
-    {
-        double temperature = 0;
-        double humidity = 0;
-        double pressure = 0;
+    const SensorEntry sensorAddresses[] = {
+        {Sensor::S_SHT4x,  0x44},
+        {Sensor::S_SHT4x,  0x45},
+        {Sensor::S_SHT4x,  0x46},
+        {Sensor::S_BME280, 0x76},
+        {Sensor::S_BME280, 0x77},
+        {Sensor::S_SCD4x,  0x62},
+        {Sensor::S_STCC4,  0x64},
+        {Sensor::S_STCC4,  0x65},
+        {Sensor::S_SGP41,  0x59},
+    };
+    constexpr size_t SENSOR_COUNT = sizeof(sensorAddresses) / sizeof(sensorAddresses[0]);
+
+
+    struct ScanResult {
+        SensorMask mask = MASK_NONE;
+        uint8_t addresses[static_cast<uint8_t>(Sensor::S_COUNT)] = {};
     };
 
-    struct SCD4xReading
-    {
-        double temperature = 0;
-        double humidity = 0;
-        double co2 = 0;
-    };
 
-    struct STCC4Reading
+    ScanResult scanI2CSensors(TwoWire &wire = Wire, uint32_t timeoutMs = 10)
     {
-        double temperature = 0;
-        double humidity = 0;
-        double co2 = 0;
-    };
+        ScanResult result;
+        wire.setTimeOut(timeoutMs);
 
-    struct SGP41Reading
-    {
-        uint16_t voc;
-        uint16_t nox;
-    };
+        for (size_t i = 0; i < SENSOR_COUNT; ++i)
+        {
+            const uint8_t idx = static_cast<uint8_t>(sensorAddresses[i].sensor);
+            if (result.mask & sensorBit(sensorAddresses[i].sensor)) continue;   // already found this sensor type
 
-    struct BH1750Reading
-    {
-        double lux = 0;
-    };
+            wire.beginTransmission(sensorAddresses[i].address);
+            if (wire.endTransmission(true) == 0) {
+                result.mask |= sensorBit(sensorAddresses[i].sensor);
+                result.addresses[idx] = sensorAddresses[i].address;
+            }
+        }
+        return result;
+    }
 
     struct SensorReading
     {
-        int flag = Sensor::_NONE;
-        SHT4xReading sht;
-        BME280Reading bme;
-        SCD4xReading scd;
-        STCC4Reading stcc4;
-        SGP41Reading sgp41;
-        BH1750Reading bh1750;
+        SensorMask mask = MASK_NONE;
+        struct {
+            double temperature = 0;
+            double humidity = 0;
+        } sht;
+
+        struct {
+            double temperature = 0;
+            double humidity = 0;
+            double pressure = 0;
+        } bme;
+
+        struct {
+            double temperature = 0;
+            double humidity = 0;
+            double co2 = 0;
+        } scd;
+
+        struct {
+            double temperature = 0;
+            double humidity = 0;
+            double co2 = 0;
+        } stcc4;
+
+        struct {
+            uint16_t voc = 0;
+            uint16_t nox = 0;
+        } sgp41;
+
     };
 
-    bool readSHT4x(SensorReading& reading)
+    bool readSHT4x(SensorReading& reading, uint8_t address)
     {
-        sht4.begin(Wire, SHT40_I2C_ADDR_44);
+        sht4.begin(Wire, address);
 
         float temp = 0.0f, hum = 0.0f;
         uint16_t err = sht4.measureLowestPrecision(temp, hum);
@@ -88,23 +131,23 @@ namespace LaskaKit::ZivyObraz {
 
         reading.sht.temperature = temp;
         reading.sht.humidity = hum;
-        reading.flag |= Sensor::_SHT4x;
+        reading.mask |= MASK_SHT4x;
         return true;
     }
 
-    bool readBME280(SensorReading& reading)
+    bool readBME280(SensorReading& reading, uint8_t address)
     {
-        if (!bme.begin()) {
+        if (!bme.begin(address)) {
             return false;
         }
         reading.bme.temperature = bme.readTemperature();
         reading.bme.humidity = bme.readHumidity();
         reading.bme.pressure = bme.readPressure() / 100.0;
-        reading.flag |= Sensor::_BME280;
+        reading.mask |= MASK_BME280;
         return true;
     }
 
-    bool readSCD4x(SensorReading& reading)
+    bool readSCD4x(SensorReading& reading, uint8_t address)
     {
         if (!SCD4.begin(false, true, false)) {
             return false;
@@ -117,14 +160,14 @@ namespace LaskaKit::ZivyObraz {
         reading.scd.temperature = SCD4.getTemperature();
         reading.scd.humidity = SCD4.getHumidity();
         reading.scd.co2 = SCD4.getCO2();
-        reading.flag |= Sensor::_SCD4x;
+        reading.mask |= MASK_SCD4x;
         return true;
     }
 
     // Single shot measurement
-    bool readSTCC4(SensorReading& reading)
+    bool readSTCC4(SensorReading& reading, uint8_t address)
     {
-        stcc4.begin(Wire, STCC4_I2C_ADDR_64);
+        stcc4.begin(Wire, address);
         delay(6);
 
         // Ensure sensor is in idle state
@@ -135,7 +178,7 @@ namespace LaskaKit::ZivyObraz {
         error = stcc4.startContinuousMeasurement();
         if (error) { return false; }
         log_i("Waiting 30s to warmup STCC4");
-        delay(30 * 1000);
+        delay(30 * 1000); // TODO wait in light sleep
         log_i("STCC4 warmup complete");
 
         error = stcc4.stopContinuousMeasurement();
@@ -156,7 +199,7 @@ namespace LaskaKit::ZivyObraz {
         error = stcc4.enterSleepMode();
         if (error) { return false; }
 
-        reading.flag |= Sensor::_STCC4;
+        reading.mask |= MASK_STCC4;
         reading.stcc4.temperature = static_cast<double>(temperature);
         reading.stcc4.humidity = static_cast<double>(humidity);
         reading.stcc4.co2 = static_cast<double>(co2);
@@ -165,7 +208,7 @@ namespace LaskaKit::ZivyObraz {
         return true;
     }
 
-    bool readSGP41(SensorReading& reading)
+    bool readSGP41(SensorReading& reading, uint8_t address)
     {
         sgp41.begin(Wire);
 
@@ -193,56 +236,50 @@ namespace LaskaKit::ZivyObraz {
 
         reading.sgp41.voc = srawVoc;
         reading.sgp41.nox = srawNox;
-        reading.flag |= Sensor::_SGP41;
+        reading.mask |= MASK_SGP41;
         return true;
     }
-
-    bool readBH1750(SensorReading& reading)
-    {
-        if (!bh1750.begin()) {
-            return false;
-        }
-        // wait for sensor to get ready
-        delay(1000);
-        float lux = bh1750.readLightLevel();
-        reading.bh1750.lux = static_cast<double>(lux);
-        reading.flag |= Sensor::_BH1750;
-        return true;
-    }
-
 
     SensorReading readSensors()
     {
         SensorReading reading;
-        readSHT4x(reading);
-        readBME280(reading);
-        readSCD4x(reading);
-        readSTCC4(reading);
-        readSGP41(reading);
-        readBH1750(reading);
+        ScanResult scanResult = scanI2CSensors();
+
+        if (scanResult.mask & MASK_SHT4x) {
+            readSHT4x(reading, scanResult.addresses[static_cast<uint8_t>(Sensor::S_SHT4x)]);
+        }
+        if (scanResult.mask & MASK_BME280) {
+            readBME280(reading, scanResult.addresses[static_cast<uint8_t>(Sensor::S_BME280)]);
+        }
+        if (scanResult.mask & MASK_SCD4x) {
+            readSCD4x(reading, scanResult.addresses[static_cast<uint8_t>(Sensor::S_SCD4x)]);
+        }
+        if (scanResult.mask & MASK_STCC4) {
+            readSTCC4(reading, scanResult.addresses[static_cast<uint8_t>(Sensor::S_STCC4)]);
+        }
+        if (scanResult.mask & MASK_SGP41) {
+            readSGP41(reading, scanResult.addresses[static_cast<uint8_t>(Sensor::S_SGP41)]);
+        }
         return reading;
     }
 
     void printSensors(SensorReading& reading)
     {
         log_i("Detected sensors:");
-        if (reading.flag & Sensor::_SHT4x) {
+        if (reading.mask & MASK_SHT4x) {
             log_i("SHT4x -> T: %f H: %f", reading.sht.temperature, reading.sht.humidity);
         }
-        if (reading.flag & Sensor::_BME280) {
+        if (reading.mask & MASK_BME280) {
             log_i("BME280 -> T: %f H: %f P: %f", reading.bme.temperature, reading.bme.humidity, reading.bme.pressure);
         }
-        if (reading.flag & Sensor::_SCD4x) {
+        if (reading.mask & MASK_SCD4x) {
             log_i("SCD4x -> T: %f H: %f CO2: %f", reading.scd.temperature, reading.scd.humidity, reading.scd.co2);
         }
-        if (reading.flag & Sensor::_STCC4) {
+        if (reading.mask & MASK_STCC4) {
             log_i("STCC4 -> T: %f H: %f CO2: %f", reading.stcc4.temperature, reading.stcc4.humidity, reading.stcc4.co2);
         }
-        if (reading.flag & Sensor::_SGP41) {
+        if (reading.mask & MASK_SGP41) {
             log_i("SGP41 -> VOC: %d NOx: %d", reading.sgp41.voc, reading.sgp41.nox);
-        }
-        if (reading.flag & Sensor::_BH1750) {
-            log_i("BH1750 -> LUX: %f", reading.bh1750.lux);
         }
     }
 }
